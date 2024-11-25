3e92ef639b2612f932ca94254833ebe407077dc8
Prabhas133 <ddhanikondaprabhas@gmail.com>
2024-11-26 00:11:11 +0530

    fix errors

--- Project1_xv6CustomizeSystemCalls/xv6-riscv/user/user_sem_prog.c
+++ Project1_xv6CustomizeSystemCalls/xv6-riscv/user/user_sem_prog.c
@@ -4,7 +4,7 @@
 
 int main() {
     struct sem mySem;  // Declare a semaphore
-    int value = 1;     // Initial value of the semaphore (you can choose based on your needs)
+    int value = 3;     // Initial value of the semaphore (you can choose based on your needs)
 
     // Initialize the semaphore
     printf("initializing semaphore...\n");
@@ -13,11 +13,13 @@ int main() {
         exit(1);
     }
     printf("Semaphore initialized successfully!\nmySem->value=%d\n\n",mySem.value);
+    while(1){
     printf("Locking...\n");
     sem_down(&mySem);  // Acquire the semaphore (this will block if value is <= 0)
     printf("\nCritical section entered\n");
     sleep(1);
-    sem_up(&mySem);  // Release the semaphore
+    //sem_up(&mySem);  // Release the semaphore
     printf("\nCritical section exited\n");
+    }
     exit(0);
 }
